6d1ea17f1bb68fc675a98cec066de48949b64de5
Vasyl Ostapenko <vasylostapenko10@gmail.com>
2021-08-14 21:14:44 -0700

    bucket change

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-s3`,
       options: {
-        bucketName: "vasyl-ostapenko",
+        bucketName: "vasylostapenko.com",
       },
     },
   ]
