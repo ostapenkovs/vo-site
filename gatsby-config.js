6ddd0c8c2dc5fe71e95889a475f8574f4030c469
Vasyl Ostapenko <vasylostapenko10@gmail.com>
2021-09-22 15:43:57 -0700

    huge changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,25 @@ module.exports = {
         hostname: "vasylostapenko.com",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-SV4ELWHJLP", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ]
 };
